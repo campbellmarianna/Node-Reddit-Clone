668a1b5f921ac3e239dfe577273a5ec5a7b5113b
Marianna <mckd2018@gmail.com>
2019-01-29 14:53:36 -0800

    add users can view all post and form validation

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,11 +1,18 @@
 // controllers/posts.js
 const Post = require("../models/post");
+const {check, validationResult } = require('express-validator/check');
 
 module.exports = router => {
     // ROOT ROUTE
     router.get('/', (req, res) => {
-        res.render('home');
-    })
+        Post.find({})
+        .then(posts => {
+            res.render("posts-index", { posts });
+        })
+        .catch(err => {
+            console.log(err.message);
+        })
+    });
 
     // NEW
     router.get('/posts/new', (req, res) => {
@@ -13,7 +20,19 @@ module.exports = router => {
     })
 
     // CREATE
-    router.post('/posts/new', (req,res) => {
+    router.post('/posts/new', [
+        // title must be at least 5 chars long
+        check('title').isLength({ min: 5}),
+        // summary must be at least 10 chars long
+        check('summary').isLength({ min: 5 })
+    ], (req,res) => {
+        // Finds the validation errors in this request and wraps them in an object with handy functions
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
+        // CHECK DATA
+        console.log(req.body)
         // INSTANTIATE INSTANCE OF POST MODEL
         const post = new Post(req.body)
         // console.log(req.body);
@@ -22,5 +41,6 @@ module.exports = router => {
             // REDIRECT TO THE ROOT
             return res.redirect('/');
         // })
+        })
     });
 };
