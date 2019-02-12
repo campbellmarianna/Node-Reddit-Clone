960387d8ee37e3175cd832fe9839ed5dfa73f6d4
Marianna <mckd2018@gmail.com>
2019-02-12 15:56:28 -0800

    add authors for posts are visible

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,15 +1,18 @@
 // controllers/posts.js
 const Post = require("../models/post");
 const {check, validationResult } = require('express-validator/check');
+const User = require('../models/user');
 
 module.exports = router => {
     // ROOT ROUTE
     router.get('/', (req, res) => {
         var currentUser = req.user;
-
-        Post.find({})
+        // res.render('home', {});
+        console.log(req.cookies)
+        Post.find().populate('author')
         .then(posts => {
             res.render("posts-index", { posts, currentUser });
+            // res.render('home', {});
         })
         .catch(err => {
             console.log(err.message);
@@ -19,18 +22,29 @@ module.exports = router => {
     // NEW
     router.get('/posts/new', (req, res) => {
         res.render('posts-new.hbs');
-    })
+    });
 
     // CREATE
     router.post('/posts/new', (req,res) => {
          if (req.user) {
              // INSTANTIATE INSTANCE OF POST MODEL
              const post = new Post(req.body);
+             post.author = req.user._id;
 
              // SAVE INSTANCE OF POST MODEL TO DB
-             post.save(function(err, post) {
-                 // REDIRECT TO THE ROOT
-                 return res.redirect('/');
+             post
+             .save()
+             .then(post => {
+                 return User.findById(req.user._id);
+             })
+             .then(user => {
+                 user.posts.unshift(post);
+                 user.save();
+                 // REDIRECT TO THE NEW POST
+                 res.redirect(`/posts/${post._id}`);
+             })
+             .catch(err => {
+                 console.log(err.message);
              });
          } else {
              return res.status(401); // UNAUTHORIZED
@@ -39,10 +53,11 @@ module.exports = router => {
 
     // SHOW
     router.get('/posts/:id', function(req, res) {
+        var currentUser = req.user;
         // LOOK UP THE POST
-        Post.findById(req.params.id).populate('comments')
+        Post.findById(req.params.id).populate('comments').populate('author')
         .then((post) => {
-            res.render("posts-show", { post });
+            res.render("posts-show", { post, currentUser });
         })
         .catch(err => {
             console.log(err.message);
@@ -51,9 +66,10 @@ module.exports = router => {
 
     // SUBREDDIT
     router.get("/n/:subreddit", function(req, res) {
-        Post.find({ subreddit: req.params.subreddit })
+        var currentUser = req.user;
+        Post.find({ subreddit: req.params.subreddit }).populate('author')
             .then(posts => {
-                res.render("posts-index", { posts });
+                res.render("posts-index", { posts, currentUser });
             })
             .catch(err => {
                 console.log(err);
