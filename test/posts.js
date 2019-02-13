78048a5d70f30084f6c2f5590ad01ee0fed67640
Marianna <mckd2018@gmail.com>
2019-02-12 19:37:29 -0800

    add implemented remaining authentication tests

--- test/posts.js
+++ test/posts.js
@@ -92,4 +92,25 @@ after(function (done) {
     .catch(function (err) {
         done(err);
     });
+
+    // LOGIN
+    it("should be able to login", function(done) {
+        agent
+            .post("/login")
+            .send({ username: "testone", password: "password" })
+            .end(function(err, res) {
+                res.should.have.status(200);
+                agent.should.have.cookie("nToken");
+                done();
+            });
+    });
+
+    // LOGOUT
+    it("should be able to logout", function(done) {
+        agent.get("/logout").end(function(err, res) {
+            res.should.have.status(200);
+            agent.should.not.have.cookie("nToken");
+            done();
+        });
+    });
 });
